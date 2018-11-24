fb22f3f15cbbdbd84901a101e249c83ba19afabf
Ankita Gupta <40393248+ankita-gupta16@users.noreply.github.com>
2018-11-24 17:34:35 +0530

    square_root.c
    Find square root of an integer without using sqrt() function.

---
+++ square_root.c
@@ -0,0 +1,16 @@
+unsigned int mySqrt(unsigned int x) {
+     
+    
+    if (x == 0 || x == 1) 
+    return x; 
+  
+    
+     int i = 1, result = 1; 
+    while (result <= x) 
+    { 
+      i++; 
+      result = i * i; 
+    } 
+    return i - 1; 
+
+}
