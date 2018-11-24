3e6745e7ff0948b4e98c5b032cb44d3b3737d66e
Ankita Gupta <40393248+ankita-gupta16@users.noreply.github.com>
2018-11-24 17:58:32 +0530

    remove element from an array
    Given an array nums and a value val, remove all instances of that value in-place and return the new length.
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    The order of elements can be changed. It doesn't matter what you leave beyond the new length.

---
+++ remove_element.c
@@ -0,0 +1,15 @@
+int removeElement(int* nums, int numsSize, int val) {
+    if(numsSize==0)
+        return 0;
+    int i,j=0;
+    
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=val)
+        {nums[j]=nums[i];
+         j++;
+    }}
+    if(j<numsSize)
+        nums[j]='\0';
+    return j;
+}
