52a0c57ccdbce3f7af7bcfc38c497ce0f281d303
Ankita Gupta <40393248+Ankitagupta16@users.noreply.github.com>
2020-05-05 21:32:11 +0530

    First Unique Character in a String

---
+++ May Leetcoding Challenge/firstUniqueChar.java
@@ -0,0 +1,17 @@
+/**
+*Given a string, find the first non-repeating character in it and return it's index. 
+*If it doesn't exist, return -1.
+*/
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] alpha = new int[26];
+        for(int i=0;i<s.length();i++){
+            alpha[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(alpha[s.charAt(i)-'a']==1)return i;
+        }
+        return -1;
+    }
+}
