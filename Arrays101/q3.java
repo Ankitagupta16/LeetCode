29c3cd4741ac58410f8a5e8754677eedb1693eec
Ankita Gupta <40393248+Ankitagupta16@users.noreply.github.com>
2020-06-15 16:49:30 +0530

    Squares of a Sorted Array

---
+++ Arrays101/q3.java
@@ -0,0 +1,22 @@
+/**
+* Given an array of integers A sorted in non-decreasing order,
+* return an array of the squares of each number, also in sorted non-decreasing order.
+*/
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
