0707c565d509cb6260055d8ec6e71631bffbeec6
Ankita Gupta <1610ankita.gupta@gmail.com>
2020-07-09 05:52:37 +0530

    Remove All Adjacent Duplicates In String

---
+++ 1047.java
@@ -0,0 +1,32 @@
+#Using string Builder
+
+class Solution {
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            int size = sb.length();
+            if (size > 0 && sb.charAt(size - 1) == c) {
+                sb.deleteCharAt(size - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
+#normal way
+
+class Solution {
+    public String removeDuplicates(String s) {
+        int i = 0, n = s.length();
+        char[] res = s.toCharArray();
+        for (int j = 0; j < n; ++j, ++i) {
+            res[i] = res[j];
+            if (i > 0 && res[i - 1] == res[i]) 
+                i -= 2;
+        }
+        return new String(res, 0, i);
+    }
+}
